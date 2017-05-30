f43390d29efd85b9732e8cd390ef17f1f64efcda
Ngọc Thiện <tranhangocthien@gmail.com>
2017-05-30 20:05:45 +0700

    commit by Thien

---
+++ check specific overall.bat
@@ -0,0 +1,29 @@
+perl conlleval.pl -l -d "\t" < 1gram/lawoutputv2.txt > 1gram/lawresultv2.txt
+perl conlleval.pl -l -d "\t" < 1gram/lawoutputv2pos.txt > 1gram/lawresultv2pos.txt
+perl conlleval.pl -l -d "\t" < 1gram/lawoutputv3.txt > 1gram/lawresultv3.txt
+perl conlleval.pl -l -d "\t" < 1gram/lawoutputv3pos.txt > 1gram/lawresultv3pos.txt
+
+perl conlleval.pl -l -d "\t" < 1gram/fineoutputv2.txt > 1gram/fineresultv2.txt
+perl conlleval.pl -l -d "\t" < 1gram/fineoutputv2pos.txt > 1gram/fineresultv2pos.txt
+perl conlleval.pl -l -d "\t" < 1gram/fineoutputv3.txt > 1gram/fineresultv3.txt
+perl conlleval.pl -l -d "\t" < 1gram/fineoutputv3pos.txt > 1gram/fineresultv3pos.txt
+
+perl conlleval.pl -l -d "\t" < 2gram/lawoutputv2.txt > 2gram/lawresultv2.txt
+perl conlleval.pl -l -d "\t" < 2gram/lawoutputv2pos.txt > 2gram/lawresultv2pos.txt
+perl conlleval.pl -l -d "\t" < 2gram/lawoutputv3.txt > 2gram/lawresultv3.txt
+perl conlleval.pl -l -d "\t" < 2gram/lawoutputv3pos.txt > 2gram/lawresultv3pos.txt
+
+perl conlleval.pl -l -d "\t" < 2gram/fineoutputv2.txt > 2gram/fineresultv2.txt
+perl conlleval.pl -l -d "\t" < 2gram/fineoutputv2pos.txt > 2gram/fineresultv2pos.txt
+perl conlleval.pl -l -d "\t" < 2gram/fineoutputv3.txt > 2gram/fineresultv3.txt
+perl conlleval.pl -l -d "\t" < 2gram/fineoutputv3pos.txt > 2gram/fineresultv3pos.txt
+
+perl conlleval.pl -l -d "\t" < 3gram/lawoutputv2.txt > 3gram/lawresultv2.txt
+perl conlleval.pl -l -d "\t" < 3gram/lawoutputv2pos.txt > 3gram/lawresultv2pos.txt
+perl conlleval.pl -l -d "\t" < 3gram/lawoutputv3.txt > 3gram/lawresultv3.txt
+perl conlleval.pl -l -d "\t" < 3gram/lawoutputv3pos.txt > 3gram/lawresultv3pos.txt
+
+perl conlleval.pl -l -d "\t" < 3gram/fineoutputv2.txt > 3gram/fineresultv2.txt
+perl conlleval.pl -l -d "\t" < 3gram/fineoutputv2pos.txt > 3gram/fineresultv2pos.txt
+perl conlleval.pl -l -d "\t" < 3gram/fineoutputv3.txt > 3gram/fineresultv3.txt
+perl conlleval.pl -l -d "\t" < 3gram/fineoutputv3pos.txt > 3gram/fineresultv3pos.txt
